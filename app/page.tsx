8d4edb32c258fcc628558beb4fb58cf45c7eead4
jackmuva <jackmu@umich.edu>
2026-07-09 16:38:28 -0400

    plaud

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Home() {
     return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans text-black">
-      PWA Demo
+      Plaud PWA Demo
     </div>
   );
 }
